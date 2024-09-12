973fc5b54bc83f1e243c02e1e57eaa20aa0533ee
Aloisio Guedes <169170722+aloisiox@users.noreply.github.com>
2024-09-12 12:38:16 -0300

    Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,18 @@
 while True:
-    numero = int(input('digite um numero: '))
-    numero2 = int(input('digite outro numero: '))
-    escolha = input('escolha se deseja somar,subtrair, multiplicar ou dividir: \n S: soma \n sub: subtração \n mult:multiplicação\n div: divisao \n ')
-
-    if escolha == 'S':
+    numero = float(input('digite um numero: '))
+    numero2 = float(input('digite outro numero: '))
+    escolha = input('escolha se deseja somar,subtrair, multiplicar ou dividir: \n 1: soma \n 2: subtração \n 3: multiplicação\n 4: divisao \n ')
+    
+    if escolha == 1:
         resultado = numero + numero2
         print(f'soma será: {resultado}')
-    elif escolha == 'sub':
+    elif escolha == 2:
         resultado = numero - numero2
         print(f'a subtração será: {resultado}')
-    elif escolha == 'mult':
+    elif escolha == 3:
         resultado = numero * numero2
         print(f'a multiplicação será: {resultado}')
-    elif escolha == 'div':
+    elif escolha == 4:
         resultado = numero / numero2
         print(f'a divisao será: {resultado}')
     else:
